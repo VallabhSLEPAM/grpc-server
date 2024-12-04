f0f60d69721214d9a81478133065f46ca165a648
VallabhSLEPAM <vallabhshivahar_lakade@epam.com>
2024-12-04 13:06:10 +0530

    feat: added code for streaming data to a client stream

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/VallabhSLEPAM/grpc-server
 go 1.23.2
 
 require (
-	github.com/VallabhSLEPAM/go-with-grpc v0.0.2
+	github.com/VallabhSLEPAM/go-with-grpc v0.0.5
 	google.golang.org/grpc v1.68.0
 )
 
 require (
-	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 )
--- internal/adapters/grpc/hello_adapter.go
+++ internal/adapters/grpc/hello_adapter.go
@@ -2,8 +2,13 @@ package grpc
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
+	"time"
 
 	"github.com/VallabhSLEPAM/go-with-grpc/protogen/go/hello"
+	"google.golang.org/grpc"
 )
 
 func (adapter *GRPCAdapter) SayHello(ctx context.Context, req *hello.HelloRequest) (*hello.HelloResponse, error) {
@@ -13,5 +18,65 @@ func (adapter *GRPCAdapter) SayHello(ctx context.Context, req *hello.HelloReques
 	return &hello.HelloResponse{
 		Greet: greet,
 	}, nil
+
+}
+
+func (adapter *GRPCAdapter) HelloServerStream(req *hello.HelloRequest, ss grpc.ServerStreamingServer[hello.HelloResponse]) error {
+
+	for i := 0; i < 10; i++ {
+		greet := adapter.helloService.GenerateHello(req.Name)
+		res := fmt.Sprintf("[%d] %s", i, greet)
+		ss.Send(
+			&hello.HelloResponse{
+				Greet: res,
+			},
+		)
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
+
+func (adapter *GRPCAdapter) HelloClientStream(clientStreamServer grpc.ClientStreamingServer[hello.HelloRequest, hello.HelloResponse]) error {
+	res := ""
+
+	for {
+		req, err := clientStreamServer.Recv()
+		if err == io.EOF {
+			return clientStreamServer.SendAndClose(
+				&hello.HelloResponse{
+					Greet: res,
+				},
+			)
+		}
+		if err != nil {
+			log.Fatalln("Error receiving data: ", err)
+		}
+		greet := adapter.helloService.GenerateHello(req.Name)
+		res += greet + " "
+	}
+}
+
+func (adapter *GRPCAdapter) HelloContinuous(stream grpc.BidiStreamingServer[hello.HelloRequest, hello.HelloResponse]) error {
+
+	for {
+
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalln("Error receiiving data: ", err)
+		}
+
+		greet := adapter.helloService.GenerateHello(req.Name)
+
+		err = stream.Send(&hello.HelloResponse{
+			Greet: greet,
+		})
+		if err != nil {
+			log.Fatalln("Error sending data to client: ", err)
+		}
+	}
 
 }
